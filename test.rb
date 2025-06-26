c921fe39f59e67f9913f16ba93ce196826d24d8d
Mirosław Pragłowski <m@praglowski.com>
2025-06-26 17:16:31 +0200

    Better test messages

--- test.rb
+++ test.rb
@@ -28,7 +28,11 @@ class Test
   def run(api)
     api.store.append(events) unless events&.empty?
     api.call(command)
-    check_expected_event(api.store.read.last)
+    if expected_error
+      fail("Expected error not raised")
+    else
+      check_expected_event(api.store.read.last)
+    end
   rescue Api::Error => e
     check_expected_error(e)
   rescue => e
@@ -38,31 +42,31 @@ class Test
   private
 
   def pass(message = nil)
-    log("PASS", description, (message && "Expected error:"), message)
+    log("PASS", description, message && ":", message)
   end
 
   def fail(message = nil)
-    log("FAIL", description, (message && "Error:"), message)
+    log("FAIL", description, message && ":", message)
   end
 
   def log(*values)
-    puts values.join(" ")
+    puts values.compact.join(" ")
   end
 
   def check_expected_event(actual_event)
     if expected_event && actual_event.instance_of?(expected_event.class) &&
          actual_event.data == expected_event.data
-      pass
+      pass("Event published as expected")
     else
-      fail
+      fail("Expected event not published")
     end
   end
 
   def check_expected_error(actual_error)
     if !expected_event && actual_error.message == expected_error
-      pass(expected_error)
+      pass("Expected error: " + expected_error)
     else
-      fail(actual_error.message)
+      fail("Unexpected error: " + actual_error.message)
     end
   end
 end
